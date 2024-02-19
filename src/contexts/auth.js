dfc75c2f1e3f6015a969c9932d69973960d78221
programadoreduardo <programador.eduardo@gmail.com>
2024-02-19 14:01:15 -0300

    adicionado o backend

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,4 +1,6 @@
 import { createContext, useState } from "react";
+import api from "../services/api";
+import { useNavigation } from "@react-navigation/native";
 
 export const AuthContext = createContext({});
 
@@ -7,8 +9,25 @@ function AuthProvider({ children }) {
         nome: 'Eduardo Teste'
     });
 
+    const navigation = useNavigation()
+
+    async function signUp(email, password, nome) {
+        try {
+            const response = await api.post('/users', {
+                name: nome,
+                password: password,
+                email: email
+            })
+            
+            navigation.goBack();
+
+        } catch (err) {
+            console.log('Erro ao cadastrar', err)
+        }
+    }
+
     return (
-        <AuthContext.Provider value={{user}}>
+        <AuthContext.Provider value={{ user, signUp }}>
             {children}
         </AuthContext.Provider>
     )
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,15 +1,18 @@
 import { KeyboardAvoidingView, Text, View, TextInput, TouchableOpacity } from 'react-native';
 import { Platform } from 'react-native';
 import styles from '../SignIn/styles';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { AuthContext } from '../../contexts/auth';
 
 export default function SignUp() {
 
-    const { user } = useContext(AuthContext)
+    const { signUp } = useContext(AuthContext)
+    const [nome, setNome] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
 
-    function handleSignUp(){
-        console.log(user.nome)
+    function handleSignUp() {
+        signUp(email, password, nome);
     }
 
     return (
@@ -21,13 +24,23 @@ export default function SignUp() {
                 <View style={styles.viewInput}>
 
                     <TextInput style={styles.inputEmail}
-                        placeholder='Nome' />
+                        placeholder='Nome'
+                        value={nome}
+                        onChangeText={(text) => setNome(text)}
+                    />
 
                     <TextInput style={styles.inputEmail}
-                        placeholder='E-mail' />
+                        placeholder='E-mail'
+                        value={email}
+                        onChangeText={(text) => setEmail(text)}
+                    />
 
                     <TextInput style={styles.inputEmail}
-                        placeholder='Senha' />
+                        placeholder='Senha'
+                        value={password}
+                        onChangeText={(text) => setPassword(text)}
+                        secureTextEntry={true}
+                    />
 
                     <TouchableOpacity style={styles.button} activeOpacity={0.8} onPress={handleSignUp}>
                         <Text style={styles.textButton}> Cadastrar</Text>
