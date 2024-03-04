25f787a132d74839d90f0cfbdf9061c63bc8de1c
programadoreduardo <programador.eduardo@gmail.com>
2024-03-04 12:00:43 -0300

    add moviments

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,35 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Button, Text, View } from "react-native";
 import { AuthContext } from "../../contexts/auth";
 import { styles } from "./styles";
 import Header from "../../components/Header";
+import { format } from "date-fns";
+import api from "../../services/api";
 
 
 export default function Home() {
-    const { singOut, user } = useContext(AuthContext)
+    const [listBalance, setListBalance] = useState([])
+    const [dateMovements, setDateMovements] = useState(new Date())
+
+    useEffect(() => {
+        let isActive = true;
+
+        async function getMovements() {
+            let dateFormated = format(dateMovements, 'dd/MM/yyyy')
+
+            const balance = await api.get('/balance', {
+                params:{
+                    date:dateFormated
+                }
+            })
+            if(isActive){
+                setListBalance(balance.data);
+            }
+        }
+        getMovements()
+
+        return()=>isActive = false;
+    }, [])
 
     return (
         <View style={styles.container}>
