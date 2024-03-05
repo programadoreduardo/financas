8add34ad1c71af20a82fbe11925a1a9cc37e075d
programadoreduardo <programador.eduardo@gmail.com>
2024-03-05 09:47:58 -0300

    creating recipes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,16 @@
 import { useContext, useEffect, useState } from "react";
-import { Button, Text, View } from "react-native";
+import { Button, FlatList, Text, View } from "react-native";
 import { AuthContext } from "../../contexts/auth";
 import { styles } from "./styles";
 import Header from "../../components/Header";
 import { format } from "date-fns";
 import api from "../../services/api";
+import { useIsFocused } from "@react-navigation/native";
+import BalanceItem from "../../components/BalanceItem";
 
 
 export default function Home() {
+    const isFocused = useIsFocused()
     const [listBalance, setListBalance] = useState([])
     const [dateMovements, setDateMovements] = useState(new Date())
 
@@ -29,11 +32,19 @@ export default function Home() {
         getMovements()
 
         return()=>isActive = false;
-    }, [])
+    }, [isFocused])
 
     return (
         <View style={styles.container}>
             <Header title="Minhas movimentações" />
+
+            <FlatList
+            data ={listBalance}
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+            keyExtractor={item => item.tag}
+            renderItem={({item})=>(<BalanceItem data={item}/>)}
+            />
         </View>
     )
 }
